449f7e123511d6c744679662bde1713b68a52cd0
Stella <unicusstella@gmail.com>
2022-07-08 14:11:07 +0900

    소수찾기 2022-07-08

---
+++ Programmers/Level_1/소수찾기.js
@@ -0,0 +1,16 @@
+function solution(n) {
+  let arr = [];
+  for (let i = 1; i <= n; i += 2) arr.push(i);
+
+  function prime(a) {
+    for (let i = 3; i <= Math.sqrt(a); i += 2) {
+      if (a % i == 0) return false;
+    }
+    return true;
+  }
+
+  let answer = arr.filter(prime).length;
+  return answer;
+}
+
+console.log(solution(5)); //3 (2,3,5)
