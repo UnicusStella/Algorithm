77ec835a719f33e0aad46d2abaa291ab7d5776ea
Stella <UnicusStella@gmail.com>
2023-08-30 13:10:06 +0900

    진료순서 정하기 2023-08-30 [HashMap, Reverse]
     [HashMap]
     * 주석 참조

--- Programmers_JAVA/src/Main.java
+++ Programmers_JAVA/src/Main.java
@@ -1,28 +1,25 @@
-import Agorithm.Programmers.Level_1.문자_반복_출력하기;
-import Agorithm.Programmers.Level_1.배열_자르기;
+import Agorithm.Programmers.Level_1.진료순서_정하기;
 
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
 
-        배열_자르기 stella = new 배열_자르기();
+        진료순서_정하기 stella = new 진료순서_정하기();
 
-        int[] numbers1 = {1, 2, 3, 4, 5};
-        int num1_1 = 1;
-        int num1_2 = 3;
-
-        int[] numbers2 = {1, 3, 5};
-        int num2_1 = 1;
-        int num2_2 = 2;
+        int[] emergency1 = {3, 76, 24};
+        int[] emergency2 = {1, 2, 3, 4, 5, 6, 7};
+        int[] emergency3 = {30, 10, 23, 6, 100};
 
 
-        System.out.println("Answer1 = " + Arrays.toString(stella.solution(numbers1, num1_1, num1_2)));
-        System.out.println("Answer1 = " + Arrays.toString(stella.solution(numbers2, num2_1, num2_2)));
+        System.out.println("Answer1 = " + Arrays.toString(stella.solution(emergency1)));
+        System.out.println("Answer1 = " + Arrays.toString(stella.solution(emergency2)));
+        System.out.println("Answer1 = " + Arrays.toString(stella.solution(emergency3)));
     }
 }
 /*
-        numbers	        num1    num2	result
-        [1, 2, 3, 4, 5]	    1	    3	[2, 3, 4]
-        [1, 3, 5]	        1	    2	[3, 5]
+        emergency	            result
+        [3, 76, 24]	            [3, 1, 2]
+        [1, 2, 3, 4, 5, 6, 7]	[7, 6, 5, 4, 3, 2, 1]
+        [30, 10, 23, 6, 100]	[2, 4, 3, 5, 1]
  */
