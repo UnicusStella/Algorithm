1a4609472887b768f496f4084c7c93aac184a9f1
Stella <UnicusStella@gmail.com>
2023-09-18 22:07:14 +0900

    Refactor: 진료순서 정하기 2023-09-18
    # For 문 => Stream 으로 변경

--- Programmers_JAVA/src/Agorithm/Programmers/Level_1/진료순서_정하기.java
+++ Programmers_JAVA/src/Agorithm/Programmers/Level_1/진료순서_정하기.java
@@ -26,31 +26,19 @@ public class 진료순서_정하기 {
      */
     public int[] solution(int[] emergency) {
 
-        // answer size 초기화
-        int[] answer = new int[emergency.length];
+        List<Integer> list = Arrays.stream(emergency)
+                .boxed()
+                .sorted(Collections.reverseOrder())
+                .collect(Collectors.toList());
 
-        // int[] 을 reverse 하기 위해서 list emergency 를 list화 시키기
-        ArrayList<Integer> arrayList = new ArrayList<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
 
-        // list 안에 Value 삽입
-        for (Integer number : emergency) {
-            arrayList.add(number);
+        for (int i = 0; i < list.size(); i++) {
+            map.put(list.get(i), i + 1);
         }
 
-        // 응급도 순서를 아기위해 내림차순 정렬
-        List<Integer> collect = arrayList.stream().sorted(Collections.reverseOrder()).collect(Collectors.toList());
-
-        Map<Integer, Integer> map = new HashMap<>();
-
-        // 내림 차순 배열의 Value 값으 Key 값으로 map 에 저장
-        for (int i = 0; i < collect.size(); i++) {
-            map.put(collect.get(i), i + 1);
-        }
-
-        // 배열 emergency 를 돌면서 emergency[i] 해당하는 map Key 값을 조회
-        for (int i = 0; i < map.size(); i++) {
-            answer[i] = map.get(emergency[i]);
-        }
-        return answer;
+        return Arrays.stream(emergency)
+                .map(item -> map.get(item))
+                .toArray();
     }
 }
