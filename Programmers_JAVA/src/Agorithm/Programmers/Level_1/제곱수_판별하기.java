e4fa3e6e37ddb05ff60fae0724d53660d766221b
Stella <UnicusStella@gmail.com>
2023-12-19 15:50:01 +0900

    세균 증식 2023-12-19 [while]
     - while 문을 이용하여 2배씩 증가하는 문제 해결

--- Programmers_JAVA/src/Agorithm/Programmers/Level_1/제곱수_판별하기.java
+++ Programmers_JAVA/src/Agorithm/Programmers/Level_1/제곱수_판별하기.java
@@ -5,21 +5,28 @@ public class 제곱수_판별하기 {
 
         제곱수_판별하기 stella = new 제곱수_판별하기();
 
-        int n1 = 144;
+        int n1 = 2;
+        int t1 = 10;
 
-        int n2 = 976;
+        int n2 = 7;
+        int t2 = 15;
 
-        System.out.println("Answer = " + stella.solution(n1));
-        System.out.println("Answer = " + stella.solution(n2));
+        System.out.println("Answer = " + stella.solution(n1, t1));
+        System.out.println("Answer = " + stella.solution(n2, t2));
     }
 
     /*
-        n	    result
-        144	    1
-        976	    2
+        n	t	result
+        2	10	2048
+        7	15	229,376
      */
-    public int solution(int n) {
+    public int solution(int n, int t) {
         int answer = 0;
-        return Math.sqrt(n) == (int) (Math.sqrt(n))  ? 1 : 2;
+
+        while(t > 0) {
+            n *= 2;
+            t--;
+        }
+        return n;
     }
 }
