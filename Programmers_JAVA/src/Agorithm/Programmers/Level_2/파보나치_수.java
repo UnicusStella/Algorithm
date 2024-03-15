5ef858a8b767f0335263ac80ec91b6aa6aac8f4e
Stella <UnicusStella@gmail.com>
2024-03-15 13:41:50 +0900

    파보나치 수 2024-03-15 [ ★파보나치 ]
    [ ★파보나치 ]
      1) 재귀함수를 이용하여 문제 해결
        - 해당 방식은 n의 값이 20만 넘어가도 연산에 과부화가 생김.
      2) DP (Dynamic Programming)
        - 동적 계획법을 이용하여 재귀호출을 효율적으로 문제 해결.
      3) 파보나치 문제에 한하여 배열을 활용하여 순차적으로 더해가며 답을 구함.

---
+++ Programmers_JAVA/src/Agorithm/Programmers/Level_2/파보나치_수.java
@@ -0,0 +1,58 @@
+package Agorithm.Programmers.Level_2;
+
+public class 파보나치_수 {
+    public int[] DP = new int[100_000];
+
+    public static void main(String[] args) {
+
+        파보나치_수 stella = new 파보나치_수();
+
+        int n1 = 3;
+        int n2 = 5;
+
+        System.out.println("Answer = " + stella.solution(n1));
+        System.out.println("Answer = " + stella.solution(n2));
+    }
+
+    /*
+        n	return
+        3	2
+        5	5
+     */
+    public int solution(int n) {
+
+        DP[1] = 1;
+        DP[0] = 0;
+
+        return Fibo(n);
+    }
+
+    private int Fibo(int n) {
+
+        if (n < 2) return n;
+
+        return Fibo(n - 1) + Fibo(n - 2);
+    }
+
+    private int FiboDP(int n) {
+
+        if (n < 2) return n;
+
+        if (DP[n] > 0) return DP[n];
+
+        DP[n] = Fibo(n - 1) + Fibo(n - 2);
+
+        return DP[n];
+    }
+
+    private int FiboArr(int n) {
+
+        int[] arr = new int[n + 1];
+        arr[1] = 1;
+
+        for (int i = 2; i < arr.length; i++)
+            arr[i] = (arr[i - 2] + arr[i - 1]) % 1234567;
+
+        return arr[n];
+    }
+}
